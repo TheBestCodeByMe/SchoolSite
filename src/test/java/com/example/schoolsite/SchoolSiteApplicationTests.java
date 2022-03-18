5304f8c553f916e35d93f40856bbfcd5b25f2d37
Uliana Kolosovich <90652401+TheBestCodeByMe@users.noreply.github.com>
2022-03-19 01:36:40 +0300

    Initial commit

---
+++ src/test/java/com/example/schoolsite/SchoolSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
